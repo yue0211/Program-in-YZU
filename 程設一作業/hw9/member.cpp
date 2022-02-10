a99514b4d035ad7f7997ff71a493c39fc2dbf328
yue0211 <82385589+yue0211@users.noreply.github.com>
2022-02-10 12:40:32 +0800

    Delete 程設一作業 directory

--- 程設一作業/hw9/member.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include<fstream>
-using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv)
-{
-	ofstream a("Members.dat",ios::out);
-}
